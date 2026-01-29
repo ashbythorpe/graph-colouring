a50d8d0eee9d3565b1e8cd010413fa3aba3f7375
Ashby Thorpe <ashbythorpe@gmail.com>
2026-01-29 20:27:41 +0000

    implement memory tracking

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,92 @@ public:
   size_t edges;
 };
 
+static bool benchmark_memory;
+
+class MemoryTracker {
+  size_t current_usage{0};
+  size_t peak_usage{0};
+  size_t total_usage{0};
+
+public:
+  void add(size_t n) {
+    current_usage += n;
+    total_usage += n;
+    if (current_usage > peak_usage) {
+      peak_usage = current_usage;
+    }
+  }
+
+  void remove(size_t n) { current_usage -= n; }
+
+  void reset() {
+    current_usage = 0;
+    peak_usage = 0;
+    total_usage = 0;
+  }
+
+  void print_usage() {
+#ifdef BENCHMARK_MEMORY
+    std::cout << "Peak memory usage: " << peak_usage / 1000000 << "MB"
+              << std::endl;
+    std::cout << "Total allocated memory: " << total_usage / 1000000 << "MB"
+              << std::endl;
+#endif // BENCHMARK_MEMORY
+  }
+};
+
+static MemoryTracker memory_tracker;
+
+#ifdef BENCHMARK_MEMORY
+// https://en.cppreference.com/w/cpp/memory/new/operator_new
+void *operator new(std::size_t sz) {
+  if (sz == 0) {
+    ++sz; // avoid std::malloc(0) which may return nullptr on success
+  }
+
+  if (void *ptr = std::malloc(sz)) {
+    memory_tracker.add(sz);
+    return ptr;
+  }
+
+  throw std::bad_alloc{}; // required by [new.delete.single]/3
+}
+
+// no inline, required by [replacement.functions]/3
+void *operator new[](std::size_t sz) {
+  if (sz == 0) {
+    ++sz; // avoid std::malloc(0) which may return nullptr on success
+  }
+
+  if (void *ptr = std::malloc(sz)) {
+    memory_tracker.add(sz);
+    return ptr;
+  }
+
+  throw std::bad_alloc{}; // required by [new.delete.single]/3
+}
+
+void operator delete(void *ptr) noexcept {
+  std::cerr << "delete without specified size" << std::endl;
+  std::free(ptr);
+}
+
+void operator delete(void *ptr, std::size_t size) noexcept {
+  memory_tracker.remove(size);
+  std::free(ptr);
+}
+
+void operator delete[](void *ptr) noexcept {
+  std::cerr << "delete[] without specified size" << std::endl;
+  std::free(ptr);
+}
+
+void operator delete[](void *ptr, std::size_t size) noexcept {
+  memory_tracker.remove(size);
+  std::free(ptr);
+}
+#endif // BENCHMARK_MEMORY
+
 Header read_header(std::istream &is) {
   std::string buf;
   size_t nodes, edges;
@@ -516,7 +602,7 @@ int main(int argc, char *argv[]) {
   // }
 
   for (size_t n = 50; n > 1; n--) {
-    g_tracker.reset();
+    memory_tracker.reset();
     auto start = std::chrono::high_resolution_clock::now();
 
     MMapReader reader{file};
@@ -530,7 +616,7 @@ int main(int argc, char *argv[]) {
               << colouring.num_colors << " colours" << std::endl;
     std::cout << "Vertices: " << colouring.colours.size() << std::endl;
     std::cout << "Time taken: " << elapsed.count() << std::endl;
-    g_tracker.print_usage();
+    memory_tracker.print_usage();
   }
 
   return 0;
